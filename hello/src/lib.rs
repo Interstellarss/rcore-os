7fcbae3575638651f571a1bcfb5cf37a6b2ee9e2
Puxuan Wang <wangpu@in.tum.de>
2025-11-05 12:45:12 +0100

    add keyboard input

---
+++ hello/src/lib.rs
@@ -0,0 +1,20 @@
+#![no_std]
+#![feature(abi_x86_interrupt)]
+
+pub mod interrupts;
+pub mod vga_buffer;
+
+pub mod gdt;
+
+pub fn init(){
+    gdt::init();
+    interrupts::init_idt();
+    unsafe {interrupts::PICS.lock().initialize()};
+    x86_64::instructions::interrupts::enable();
+}
+
+pub fn hlt_loop() -> ! {
+    loop{
+        x86_64::instructions::hlt();
+    }
+}
--- hello/src/main.rs
+++ hello/src/main.rs
@@ -4,8 +4,9 @@
 
 // #![feature(custom_test_frameworks)]
 // #![test_runner(crate::test_runner)]
-mod vga_buffer;
+//mod vga_buffer;
 use core::panic::PanicInfo;
+use rosh::println;
 // use vga_buffer::Writer;
 // use vga_buffer::Color;
 // use vga_buffer::ColorCode;
@@ -16,7 +17,7 @@ use core::panic::PanicInfo;
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     println!("{}", _info);
-    loop {}
+    rosh::hlt_loop();
 }
 
 
@@ -37,10 +38,16 @@ pub extern "C" fn _start() -> ! {
     // write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337);
     println!("Hello World{}", "!");
 
-    // writer.write_string("Hello this is the first line!\n");
-    // writer.write_string("Hello this is the second line!\n");
+    rosh::init();
 
+    //x86_64::instructions::interrupts::int3();
 
-    loop {}
+    // unsafe {
+    //     *(0xdeadbeef as *mut u8) = 42;
+    // }
+
+    println!("It did not crash!");
+
+    rosh::hlt_loop();
 }
 
